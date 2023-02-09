aa0dbce9851ebfffe1f60f2c454913fc12ba8029
MD Iyasin Arafat <arafat@arafat-2.local>
2023-02-09 18:32:09 +0600

    added tables fields

--- database/migrations/2023_02_09_122634_create_task_lists_table.php
+++ database/migrations/2023_02_09_122634_create_task_lists_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('task_lists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->string('title');
             $table->timestamps();
         });
     }
--- database/migrations/2023_02_09_122640_create_tasks_table.php
+++ database/migrations/2023_02_09_122640_create_tasks_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('list_id')->constrained('task_lists')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->double('position');
             $table->timestamps();
         });
     }
